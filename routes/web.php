1686aa4b7e9fde4b7de38e327135baa990f3a135
XuZhixuan <sxxuzhixuan@hotmail.com>
2019-03-16 11:13:17 +0800

    完成用户查询

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@root');
+
+Route::post('person', 'PeopleController@show')->name('person.show');
